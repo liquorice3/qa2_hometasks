8b8bc72f0e652eb64db7de18c0a551669ebdfbc2
Lyuba <lubova.kucina@gmail.com>
2025-04-02 15:46:22 +0300

    Added one more homework project

---
+++ src/test/java/javabasics/lesson4/HomeworkProject2.java
@@ -0,0 +1,37 @@
+package javabasics.lesson4;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class HomeworkProject2 {
+    @Test
+    public void printMyNameIs() {
+
+        String firstName = "Lyuba";
+        String lastName = "Kuchina";
+        byte age = 39;
+        LocalDate dateOfBirth = LocalDate.of(1985, 12, 20);
+        String email = "lubova.kucina@gmail.com";
+        String gender = "female";
+        boolean hasCar = true;
+        String personalNumber = "201285-13556";
+        String homeAddress = "Zvaigznaja gatve 16-51, Riga";
+        double heightInMeters = 1.73;
+        byte weightInKilograms = 62;
+
+        System.out.println("My name is " + firstName + " and I'm " + age + ". My birthday is " + dateOfBirth + ".");
+    }
+
+    @Test
+    public void calculateAgeInDays() {
+        LocalDate dateOfBirth = LocalDate.of(1985, 12, 20); //дата рождения
+        LocalDate currentDate = LocalDate.now(); //текущая дата
+
+        long ageInDays = ChronoUnit.DAYS.between(dateOfBirth, currentDate);
+
+        System.out.println("Возраст в днях: " + ageInDays);
+
+    }
+}
