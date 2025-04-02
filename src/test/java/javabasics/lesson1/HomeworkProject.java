8c5421aecc23823c9c203e60016af0245c6dd3f4
Lyuba <lubova.kucina@gmail.com>
2025-04-03 00:02:22 +0300

    Added a homework project on printing a line

---
+++ src/test/java/javabasics/lesson1/HomeworkProject.java
@@ -0,0 +1,10 @@
+package javabasics.lesson1;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeworkProject {
+    @Test
+     public void printHomeworkProject() {
+         System.out.println("Hi! This is a homework project!");
+     }
+}
